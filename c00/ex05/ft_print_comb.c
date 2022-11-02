0b913e422f6c846ad320ab33ca64b1830a8393b1
Mathis Serrier <maserrie@c1r3p2.42perpignan.fr>
2022-11-02 16:54:20 +0100

    c00

---
+++ c00/ex05/ft_print_comb.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maserrie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/01 19:56:39 by maserrie          #+#    #+#             */
+/*   Updated: 2022/11/01 22:30:31 by maserrie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_put3(char a, char b, char c)
+{
+	ft_putchar(a + '0');
+	ft_putchar(b + '0');
+	ft_putchar(c + '0');
+	if (a + b + c != 24)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb(void)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	a = 0;
+	while (a < 10)
+	{
+		b = a + 1;
+		while (b < 10)
+		{
+			c = b + 1;
+			while (c < 10)
+			{
+				ft_put3(a, b, c);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
---
+++ c00/ex06/ft_print_comb2.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maserrie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/01 22:06:03 by maserrie          #+#    #+#             */
+/*   Updated: 2022/11/01 22:30:51 by maserrie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a < 100)
+	{
+		b = a + 1;
+		while (b < 100)
+		{
+			ft_putchar(a / 10 + '0');
+			ft_putchar(a % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(b / 10 + '0');
+			ft_putchar(b % 10 + '0');
+			if (b + a != 197)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+}
